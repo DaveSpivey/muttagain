7cd678f4a2821ee68c0b2a1be4401150aa239c36
DaveSpivey <dspive1@gmail.com>
2017-01-13 06:41:52 -0800

    Initial home layout, make slider responsive

--- app/assets/javascripts/components/guessDisplay.jsx
+++ app/assets/javascripts/components/guessDisplay.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 
 export default class GuessDisplay extends React.Component {
 
+  constructor() {
+    super();
+    this.state = { hidden: true };
+
+    this.getTopGuesses = this.getTopGuesses.bind(this);
+    this.showGuesses = this.showGuesses.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.muttId != this.props.muttId) {
+      this.setState({ hidden: true });
+    }
+  }
+
   getTopGuesses(guesses) {
     const guessList = Object.keys(guesses).map((guessName) => {
       const currentGuess = guesses[guessName];
@@ -18,22 +32,32 @@ export default class GuessDisplay extends React.Component {
 
     return topGuesses.map((guess) => {
       return <li key={ guess.name } className="guess-item" >
-               <a href={ guess.link }>{ guess.name + ", " + guess.frequency }</a>
+               <a href={ guess.link }>{ guess.name }</a>
+               <span className="guess-frequency">{ guess.frequency }</span>
              </li>
     });
   }
 
+  showGuesses() {
+    this.setState({ hidden: false });
+  }
+
   render() {
     const { muttId, guesses } = this.props;
-    const guessList = this.getTopGuesses(guesses);
-    return (
-      <div>
-        GuessDisplay muttId: { muttId }
-        <p>Top 3 guesses:</p>
-        <ul>
-          { guessList }
-        </ul>
-      </div>
-    );
+    if (this.state.hidden) {
+      return (<div onClick={ this.showGuesses } className="guess-display guess-display-hidden">
+          <p className="top-guesses">Show top guesses for this mutt</p>
+        </div>
+      );
+    } else {
+      const guessList = this.getTopGuesses(guesses);
+      return (<div className="guess-display">
+          <p className="top-guesses">Top guesses for this mutt:</p>
+          <ul>
+            { guessList }
+          </ul>
+        </div>
+      );
+    }
   }
 };
